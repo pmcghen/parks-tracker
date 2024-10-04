4e0afebc502290f735597ccfd7d619a32cf782a2
Pat McGhen <patrickmcghen@gmail.com>
2024-10-03 20:22:19 -0400

    Add prisma

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,9 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import GitHub from "@auth/sveltekit/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "./prisma";
 
 export const { handle, signIn } = SvelteKitAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [GitHub],
 });
