98af6ccbb3daad793ff2905080a5c8bfb074847a
Pat McGhen <patrickmcghen@gmail.com>
2024-10-07 20:47:36 -0400

    Basic pagination

--- src/routes/parks/+page.server.ts
+++ src/routes/parks/+page.server.ts
@@ -1,15 +1,40 @@
 import { PARKS_API_KEY } from "$env/static/private";
 import { error } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
 
-export async function load() {
-  const parksResponse = await fetch(
-    `https://developer.nps.gov/api/v1/parks?api_key=${PARKS_API_KEY}`
-  );
+export const load: PageServerLoad = async ({ url }) => {
+  const parksPerPage = 50;
+  let pageNumber: string;
+  let parksResponse: Response;
+  const baseUrl = `https://developer.nps.gov/api/v1/parks?api_key=${PARKS_API_KEY}`;
+
+  if (url.searchParams.size && url.searchParams.get("p")) {
+    pageNumber = url.searchParams.get("p") ?? "1";
+    parksResponse = await fetch(
+      `${baseUrl}&start=${parksPerPage * (parseInt(pageNumber) - 1) + 1}`
+    );
+  } else {
+    parksResponse = await fetch(baseUrl);
+  }
+
   const parksData = await parksResponse.json();
 
+  const totalParks = parksData.total;
+
+  let pages = 0;
+
+  if (totalParks > parksPerPage) {
+    pages = Math.ceil(totalParks / parksPerPage);
+  }
+
+  console.log(pages);
+
   if (parksResponse.status === 200) {
-    return { parks: parksData.data };
+    return {
+      pages,
+      parks: parksData.data,
+    };
   }
 
   error(500, "error fetching data");
-}
+};
