c7dafe5b4cebbcd7af04fc30ddcda40afc268b99
Pat McGhen <pmcghen@proton.me>
2024-11-05 16:17:31 -0500

    Add user and visited status to park detail data

--- src/routes/parks/[park]/+page.server.ts
+++ src/routes/parks/[park]/+page.server.ts
@@ -1,8 +1,34 @@
 import { PARKS_API_KEY, WEATHER_API_KEY } from "$env/static/private";
 import { error } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
+import { prisma } from "../../../prisma";
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({ params, parent }) => {
+  const userId = (await parent()).session?.user?.id;
+  let park = await prisma.park.findFirst({
+    where: {
+      npsId: params.park,
+    },
+  });
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    include: {
+      Park_ParksVisited: true,
+    },
+  });
+
+  if (!park) {
+    park = await prisma.park.create({
+      data: {
+        npsId: params.park,
+      },
+    });
+  }
+
+  const isVisited = !!user?.Park_ParksVisited.includes(park);
+
   const parksResponse = await fetch(
     `https://developer.nps.gov/api/v1/parks?parkCode=${params.park}&api_key=${PARKS_API_KEY}`
   );
@@ -15,7 +41,13 @@ export const load: PageServerLoad = async ({ params }) => {
     );
 
     const weatherData = await weatherResponse.json();
-    return { park: parksData.data[0], weather: weatherData.current };
+
+    return {
+      park: parksData.data[0],
+      weather: weatherData.current,
+      user,
+      isVisited,
+    };
   }
 
   error(500, "error fetching data");
