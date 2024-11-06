608777a6c34b2dc1f17e8211139d66d26f3f8b1d
Pat McGhen <pmcghen@proton.me>
2024-11-06 08:38:44 -0500

    Remove API call from homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,9 @@
-<script lang="ts">
-  let { data } = $props();
-
-  const { randomPark, randomParkImage, activity } = data;
-</script>
-
 <svelte:head>
   <title>Home | National Parks Tracker</title>
 </svelte:head>
 
 <main>
-  <div class="hero" style="background-image: url({randomParkImage.url}">
+  <div class="hero">
     <div class="content">
       <h1>National Parks Tracker</h1>
       <p class="hero--details">Explore U.S. National Parks</p>
@@ -18,8 +12,7 @@
   <div class="container">
     <p>
       Whether you're taking the family out for a leisurely afternoon stroll or
-      gearing up for a weeklong {activity}
-      adventure at {randomPark.fullName}, our national parks have a lot to
+      gearing up for a weeklong adventure our national parks have a lot to
       offer. You can use the National Parks Tracker to search for your new
       favorite spot, show off which parks you've visited, or save your favorites
       to add to your bucket list!
@@ -29,7 +22,7 @@
 
 <style>
   .hero {
-    background-color: rgba(228, 235, 231, 0.5);
+    background-color: rgba(26, 43, 33, 0.8);
     background-size: cover;
     background-position: 50%;
     display: flex;
