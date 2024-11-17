b4cf6ef33ef306c67e7ed66ef644ea5141b15eaa
Pat McGhen <pmcghen@proton.me>
2024-11-17 17:34:51 -0500

    Style pagination

--- src/routes/parks/+page.svelte
+++ src/routes/parks/+page.svelte
@@ -10,6 +10,13 @@
 
   const { parks, pages } = data;
   const pageLinks: PageLink[] = [];
+  let params: URLSearchParams | undefined;
+  let currentPage = $state("1");
+
+  if (browser) {
+    params = new URLSearchParams(document.location.search);
+    currentPage = params.get("p") ?? "1";
+  }
 
   if (browser) {
     const pageUrl = new URL(window.location.href);
@@ -41,8 +48,39 @@
   {/if}
 
   {#if pageLinks.length}
-    {#each pageLinks as link}
-      <a data-sveltekit-reload href={link.url}>{link.number}</a>
-    {/each}
+    <nav aria-label="Parks pagination">
+      {#each pageLinks as link}
+        <a
+          data-sveltekit-reload
+          href={link.url}
+          class={link.number === parseInt(currentPage) ? "active" : ""}
+          >{link.number}</a
+        >
+      {/each}
+    </nav>
   {/if}
 </main>
+
+<style>
+  a {
+    border: solid 1px #999;
+    border-radius: 0.5rem;
+    display: inline-block;
+    padding: 0.5rem 1rem;
+  }
+
+  nav {
+    text-align: center;
+
+    & a:not(:first-child) {
+      margin-left: 1rem;
+    }
+  }
+
+  .active {
+    background: var(--font-color);
+    color: var(--background-color);
+    border: none;
+    text-decoration: none;
+  }
+</style>
