689003f876ddc581628d73270d2c49f6328107bd
Pat McGhen <pmcghen@proton.me>
2024-11-18 20:22:36 -0500

    Actions component scaffolding

---
+++ src/routes/parks/[park]/Actions.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+  import { enhance } from "$app/forms";
+
+  export let isVisited: boolean;
+  const user = $page.data.session?.user?.id || "";
+
+  async function toggleVisited() {
+    isVisited = !isVisited;
+  }
+</script>
+
+<div>
+  <form method="POST" action="?/updateVisitStatus" use:enhance>
+    <input type="hidden" value={user} />
+    <button onclick={toggleVisited} value={String(!isVisited)}>
+      {#if isVisited}
+        I haven't visited this park
+      {:else}
+        I've visited this park!
+      {/if}
+    </button>
+  </form>
+</div>
